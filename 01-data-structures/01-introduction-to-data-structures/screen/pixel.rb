a859a97ca0a83eafb8bff927354541e72233444d
Edward Choi <edwardchoi.2011@gmail.com>
2018-05-08 17:22:32 -0700

    pixel

--- 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
@@ -3,6 +3,7 @@
 # If  a value less than zero is specified, default to zero. If a value greater than 255 is specified, default to 255.
 # Pixels also require a coordinate location: x and y.
 
+# class called Pixel gets created that has properties red, blue, green, x, and y
 class Pixel
   attr_accessor :red
   attr_accessor :green
@@ -10,13 +11,42 @@ class Pixel
   attr_accessor :x
   attr_accessor :y
 
-
   def initialize(red, green, blue, x, y)
+    red, green, blue, x, y = [], [], [], [], []
   end
 
   private
 
   def validate_color(color)
+    if red > 0 
+      if red < 255
+        return red
+      else
+        red == 255
+      end
+    else  
+      red == 0
+    end
+
+    if blue > 0
+      if blue < 255
+        return blue
+      else 
+        blue == 255
+      end
+    else 
+      blue == 0
+    end
+
+    if green > 0
+      if green < 255
+        return green
+      else 
+        green == 255
+      end
+    else 
+      green == 0
+    end
   end
 
 end
