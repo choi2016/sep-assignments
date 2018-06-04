4b6e3fea843f01c8ab2e313ccfe0e7f1ed852d41
Edward Choi <edwardchoi.2011@gmail.com>
2018-06-04 12:32:30 -0700

    seperate chaingning tests all pass

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -4,30 +4,89 @@ class SeparateChaining
   attr_reader :max_load_factor
 
   def initialize(size)
+    @items = Array.new(size)
+    @item_count = 0
     @max_load_factor = 0.7
   end
 
   def []=(key, value)
+    i = index(key, @items.size)
+    item = Node.new(key, value)
+
+    @items[i] != nil ? list = @items[i] : list = LinkedList.new
+
+    list.add_to_tail(item)
+    @items[i] = list
+    @item_count = @item_count + 1
+
+    # Resize the hash if the load factor grows too large
+    if load_factor.to_f > max_load_factor.to_f
+      resize
+    end
   end
 
   def [](key)
+    list = @items.at(index(key, @items.size))
+    if !list.nil?
+      curr = list.head
+      while curr != nil
+        if curr.key == key
+          return curr.value
+        end
+        curr = curr.next
+      end
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    @item_count / self.size.to_f
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.size
   end
 
   # Resize the hash
   def resize
+    new_size = size*2
+    new_items = Array.new(new_size)
+    (0..@items.size-1).each do |i|
+      list = @items[i]
+      if list != nil
+        curr = list.head
+        # We only need to compute the new index once
+        new_index = index(curr.key, new_items.size)
+        while curr != nil
+          list = LinkedList.new
+          list.add_to_tail(curr)
+          new_items[new_index] = list
+          curr = curr.next
+        end
+      end
+    end
+
+    @items = new_items
+  end
+
+  def status
+    @linked_list.each do |item|
+      if item
+        current = item.head
+        while current
+          puts "Key: #{current.key}, Value: #{current.value}"
+          current = current.next
+        end
+      end
+    end
+    puts "Load Factor: #{load_factor}"
   end
 end
