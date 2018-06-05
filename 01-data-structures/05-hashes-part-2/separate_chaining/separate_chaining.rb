eb7a22cf1263d8588b3533db75866219734571ec
Edward Choi <edwardchoi.2011@gmail.com>
2018-06-05 13:07:32 -0700

    tried to print out function output

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -89,4 +89,21 @@ class SeparateChaining
     end
     puts "Load Factor: #{load_factor}"
   end
+
 end
+
+star_wars_movies = SeparateChaining.new(6)
+
+#adding entries into new hash
+star_wars_movies["Star Wars: The Phantom Menace"] = "Number One"
+star_wars_movies["Star Wars: Attack of the Clones"] = "Number Two"
+star_wars_movies["Star Wars: Revenge of the Sith"] = "Number Three"
+star_wars_movies["Star Wars: A New Hope"] = "Number Four"
+star_wars_movies["Star Wars: The Empire Strikes Back"] = "Number Five"
+star_wars_movies["Star Wars: Return of the Jedi"] = "Number Six"
+
+puts star_wars_movies.status
+
+
+
+
