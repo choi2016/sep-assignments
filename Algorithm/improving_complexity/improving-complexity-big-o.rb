4751db8107a4516fde05128a642a325bf234fdbb
Edward Choi <edwardchoi.2011@gmail.com>
2018-08-14 13:34:06 -0700

    redid third version tohave better space complexity

--- Algorithm/improving_complexity/improving-complexity-big-o.rb
+++ Algorithm/improving_complexity/improving-complexity-big-o.rb
@@ -4,23 +4,23 @@ Benchmark:
 poorly written ruby:  0.010000   0.000000   0.010000 (  0.013586)
 code optimization:  0.010000   0.000000   0.010000 (  0.009097)
 time complexity:  0.010000   0.000000   0.010000 (  0.009970)
-space complexity:  0.080000   0.000000   0.080000 (  0.075489)
+space complexity:  0.010000   0.000000   0.010000 (  0.001756)
 
 	5000
        user     system      total        real
 poorly written ruby:  0.330000   0.000000   0.330000 (  0.332027)
 code optimization:  0.220000   0.000000   0.220000 (  0.221514)
 time complexity:  0.070000   0.030000   0.100000 (  0.107468)
-space complexity:  1.990000   0.010000   2.000000 (  2.015852)
+space complexity:  0.020000   0.000000   0.020000 (  0.014775)
 
 	10000
 poorly written ruby:  1.310000   0.000000   1.310000 (  1.323629)
 code optimization:  0.870000   0.000000   0.870000 (  0.869728)
 time complexity:  0.250000   0.150000   0.400000 (  0.403418)
-space complexity:  8.600000   0.020000   8.620000 (  8.658377)
+space complexity:  0.040000   0.000000   0.040000 (  0.035511)
 
 Big-O:
 Poorly Written Ruby:    O(2^n)
 Code Optimization:      O(n^2)
 Time Complexity:        O(nlog n)
-Space Complexity:       O(1)
+Space Complexity:       O(nlog n)
--- Algorithm/improving_complexity/improving_complexity_version_three.rb
+++ Algorithm/improving_complexity/improving_complexity_version_three.rb
@@ -1,26 +1,46 @@
-def improve_space_complexity(*arrays)
-  combined_array = []
+def improve_space_complexity(*array)
+  heapify(array.flatten!)
+  last = array.length - 1
 
-  arrays.each do |array|
-    combined_array += array
+  while last > 0
+    array[last], array[0] = array[0], array[last]
+    last -= 1
+    sift_down(array, 0, last)
   end
-
-  return bubble_sort(combined_array)
+  array
 end
 
-def bubble_sort(collection)
-  n = collection.length
-  begin
-    swapped = false
+def heapify(array)
+  start = (array.length - 2) / 2
 
-    (n - 1).times do |i|
-      if collection[i] > collection[i + 1]
-        tmp = collection[i]
-        collection[i] = collection[i + 1]
-        collection[i + 1] = tmp
-        swapped = true
-      end
+  while start >= 0
+    sift_down(array, start, array.length - 1)
+    start -= 1
+  end
+  array
+end
+
+def sift_down(array, start, last)
+  root = start
+
+  while (root * 2 + 1) <= last
+    child = (root * 2 + 1)
+    temp = root
+
+    if array[temp] < array[child]
+      temp = child
     end
-  end until not swapped
-  collection
+
+    if child + 1 <= last && array[temp] < array[child + 1]
+      temp = child + 1
+    end
+
+    if temp != root
+      array[root], array[temp] = array[temp], array[root]
+      root = temp
+    else
+      return
+    end
+  end
 end
+
